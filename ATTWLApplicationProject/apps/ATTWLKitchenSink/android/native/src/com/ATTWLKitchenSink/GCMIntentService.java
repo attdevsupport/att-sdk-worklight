7bbda758d76a134a7e4441da95fcf63e9cb4adca
attdevsupport <developer.support@att.com>
2013-09-16 10:44:30 -0700

    Release 3.0 OAuth updates

--- ATTWLApplicationProject/apps/ATTWLKitchenSink/android/native/src/com/ATTWLKitchenSink/GCMIntentService.java
+++
@@ -1,5 +0,0 @@
-package com.ATTWLKitchenSink;
-
-public class GCMIntentService extends com.worklight.androidgap.push.GCMIntentService{
-	//Nothing to do here...
-}
--- ATTWLApplicationProject/apps/ATTWLKitchenSink/iphone/native/www/default/wlclient/js/checksum.js
+++
@@ -1,2 +0,0 @@
-var WL_CHECKSUM = {"checksum":1357183212,"date":1365454680411,"machine":"Giridhars-MacBook-Pro.local"};
-/* Date: Mon Apr 08 13:58:00 PDT 2013 */
